1dab5c0105fc66a3dd32e275e4ae9d9a75e4e476
ndvisme <herespier@yahoo.com>
2023-01-09 20:10:53 +0200

    Add Custom new Controller UI plus, custom layouts.

--- force-app/main/default/lwc/createnewlogg/createnewlogg.js
+++ force-app/main/default/lwc/createnewlogg/createnewlogg.js
@@ -8,7 +8,7 @@ import LOG_ONLY_FAILURE from '@salesforce/schema/Logg_Controller__c.Log_Only_Fai
 
 export default class createnewlogg extends NavigationMixin(LightningElement)  {
 
-    // objectApiName is "Order" when this component is called
+
     @api objectApiName;
     @api CardTitle;
     @track isShowModal = false;
@@ -19,38 +19,19 @@ export default class createnewlogg extends NavigationMixin(LightningElement)  {
     currentPageReference;
 
     connectedCallback() {
-        console.log(`recordTypeId = ${this.currentPageReference.state.recordTypeId}`);
         this.recordTypeId = this.currentPageReference.state.recordTypeId;
-        console.log(this.recordTypeId + this.recordTypeId);
+        console.log(this.recordTypeId);
     }
 
     handleSuccess(event) {
-        console.log(JSON.stringify(event.detail))
-        const evt = new ShowToastEvent({
-            title: 'Controller created',
-            variant: 'success',
-        });
-        this.dispatchEvent(evt);
+        console.log('onsuccess event recordEditForm',event.detail.id);
         this[NavigationMixin.Navigate]({
-            type: 'standard__recordPage',
-            attributes: {
-                recordId: event.detail.id,
-                objectApiName: 'Logg_Controller__c',
-                actionName: 'view'
-            },
+          type: 'standard__recordPage',
+          attributes: {
+            recordId: event.detail.id,
+            objectApiName: 'GRA_RV_Rig_Verification__c',
+            actionName: 'view'
+          },
         });
-    }
-    closeModal() {
-        // Navigation to Order List view(recent)
-        this[NavigationMixin.Navigate]({
-            type: 'standard__objectPage',
-            attributes: {
-                objectApiName: 'Logg_Controller__c',
-                actionName: 'list'
-            },
-            state: {
-                filterName: 'Recent'
-            },
-        });
-    }
+      }
 }
